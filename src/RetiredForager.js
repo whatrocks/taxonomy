141b041ac71ba4fedc65605f3cbbcfd6cda11594
Charles Harrington <charles.harrington@gmail.com>
2015-10-14 12:02:40 -0700

    Implement Honey, Forager, and RetiredForager .. passed all tests

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,47 @@
-var RetiredForagerBee = function() {
+/*var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+Grub.prototype.eat = function() {  
+};
+var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+};
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
+var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+};
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function(treasure) {
+  treasure = treasure || 'treasure';
+  this.treasureChest.push(treasure);
+};
+*/
+var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+};
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+};
+RetiredForagerBee.prototype.gamble = function() {
+  this.treasureChest.push('treasure');
+};
